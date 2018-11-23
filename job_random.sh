008ffa390674f82dbbc0624cb27df146f4086218
sebastianpb <33877904+sebastianpb@users.noreply.github.com>
2018-11-23 07:58:31 -0500

    Add files via upload

---
+++ job_random.sh
@@ -0,0 +1,9 @@
+#PBS -l nodes=1:ppn=1,mem=1gb,walltime=00:10:00
+#PBS -m abe
+#PBS -N ejercicio30
+
+
+module load anaconda/python3
+cd $PBS_O_WORKDIR # este es el directorio desde donde se ejecuto qsub
+gcc -fopenmp random.c -o random -lm
+./random
